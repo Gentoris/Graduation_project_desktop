e67c66c750635e282bc1f22c298d244de5c1d866
Kerecsényi Márk <karatelovag@gmail.com>
2023-02-27 23:31:14 +0100

    Szebb megjelenités, de nem tökéletes

--- src/main/java/com/example/graduation_project_desktop/DisplayCourses.java
+++ src/main/java/com/example/graduation_project_desktop/DisplayCourses.java
@@ -4,23 +4,49 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DisplayCourses {
     public static void main(String[] args) {
+        List<Course> courses = getCoursesFromBackend("http://localhost:3000/courses");
+
+        for (Course course : courses) {
+            System.out.println(course.toString());
+        }
+    }
+
+    private static List<Course> getCoursesFromBackend(String urlStr) {
+        List<Course> courses = new ArrayList<>();
+
         try {
-            URL url = new URL("http://localhost:3000/courses"); // Backend URL
+            URL url = new URL(urlStr);
             HttpURLConnection con = (HttpURLConnection) url.openConnection();
             con.setRequestMethod("GET");
-            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-            String inputLine;
-            StringBuffer response = new StringBuffer();
-            while ((inputLine = in.readLine()) != null) {
-                response.append(inputLine);
+
+            int responseCode = con.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                String inputLine;
+                StringBuilder response = new StringBuilder();
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                in.close();
+
+                String[] lines = response.toString().split("\n");
+                for (String line : lines) {
+                    String[] fields = line.split(",");
+                    if (fields.length >= 5) {
+                        Course course = new Course(fields[0], fields[1], fields[2], fields[3], fields[4]);
+                        courses.add(course);
+                    }
+                }
             }
-            in.close();
-            System.out.println(response.toString()); // Megjelenites
         } catch (Exception e) {
-            System.out.println("Váratlan hiba történt: " + e.getMessage());
+            System.out.println("Hibakeletkezett az adatok lekérdezésekor: " + e.getMessage());
         }
+
+        return courses;
     }
 }
