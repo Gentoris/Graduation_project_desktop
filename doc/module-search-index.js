eee9412859c448c246e1b302987ae41990a7d19d
Kerecsényi Márk <karatelovag@gmail.com>
2023-04-27 11:48:10 +0200

    dokumentáció

---
+++ doc/module-search-index.js
@@ -0,0 +1,1 @@
+moduleSearchIndex = [{"l":"com.example.graduation_project_desktop"}];updateSearchResults();
---
+++ doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"com.example.graduation_project_desktop","l":"com.example.graduation_project_desktop"}];updateSearchResults();
